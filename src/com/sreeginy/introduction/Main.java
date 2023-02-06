1083322f0fe1f8b7ae6a3c1cc1e7e64773b666ca
Sreeginy Kodeeswaran <sreeginy98@gmail.com>
2023-02-06 12:56:55 +0530

    intro
    class, object, variable, operator initial

---
+++ src/com/sreeginy/introduction/Main.java
@@ -0,0 +1,28 @@
+package com.sreeginy.introduction;
+
+public class Main {
+    public static void main(String[] args) {
+        //store 5 roll numbers
+        int[] numbers = new int[5];
+
+        //store 5 names
+        String[] names = new String[5];
+
+        //data of 5 students: {roll no, name, marks}
+        int[] rollno = new int[5];
+        String[] name = new String[5];
+        float[] marks = new float[5];
+
+        Student[] students = new Student[5];
+
+        Student[] Sreeginy;
+
+    }
+}
+    //class create
+    // for every single student have
+    class Student {
+        int rollno;
+        String name;
+        float marks;
+    }
