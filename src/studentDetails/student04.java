177ff78f09b6815d4aa63335b21a77d8934bc93d
Sreeginy Kodeeswaran <sreeginy98@gmail.com>
2023-02-12 20:51:28 +0530

    parameterized constructor

---
+++ src/studentDetails/student04.java
@@ -0,0 +1,15 @@
+package studentDetails;
+
+public class student04 {
+    int ID;
+    String Name;
+
+    student04(int ID, String Name) {
+        this.ID = ID;
+        this.Name = Name;
+    }
+    void display() {
+        System.out.println("ID : " + this.ID );
+        System.out.println("Name : " + this.Name);
+    }
+}
