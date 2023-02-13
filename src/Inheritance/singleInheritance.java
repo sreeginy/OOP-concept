d0d5aa5cad8b3711e0c03ce5fc35b17ca5b82f68
Sreeginy Kodeeswaran <sreeginy98@gmail.com>
2023-02-13 22:12:55 +0530

    single Inheritance

---
+++ src/Inheritance/singleInheritance.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public class singleInheritance {
+    int x;
+    singleInheritance() {
+        this.x = 10;
+    }
+    void getx() {
+        System.out.println("A" + this.x);
+    }
+}
