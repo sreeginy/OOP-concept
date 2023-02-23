3992e9f5ab700c55db69eca602b57862a72f29a3
Sreeginy Kodeeswaran <sreeginy98@gmail.com>
2023-02-23 21:59:11 +0530

    Update Students.java

--- src/Inheritance/Students.java
+++ src/Inheritance/Students.java
@@ -1,7 +1,16 @@
 package Inheritance;
 
 public class Students {
-    int x;
-    int y;
-    int z;
+    int id;
+    String name;
+
+    void setstudents() {
+        this.id = 1000;
+        this.name = "Giny";
+
+    }
+    void setstudents(int id)  {
+        this.id = id;
+        this.name = name;
+    }
 }
