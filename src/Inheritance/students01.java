cd3269153dcd56a11600b7a39b4625c74c313ec4
Sreeginy Kodeeswaran <sreeginy98@gmail.com>
2023-02-27 23:48:45 +0530

    method overloading

--- src/Inheritance/students01.java
+++ src/Inheritance/students01.java
@@ -6,5 +6,15 @@ public class students01 {
 
     students01() {
 
+        this.id =1000;
+        this.name="Giny";
+    }
+    students01(int id) {
+        this.id = id;
+        this.name ="Jos";
+    }
+
+    students01(String name) {
+
     }
 }
