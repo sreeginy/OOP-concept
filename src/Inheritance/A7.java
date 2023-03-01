f5de40f62591142dba3d175b9e6e9e66cac45a5c
Sreeginy Kodeeswaran <sreeginy98@gmail.com>
2023-03-01 23:43:35 +0530

    Create A7.java

---
+++ src/Inheritance/A7.java
@@ -0,0 +1,16 @@
+package Inheritance;
+
+public class A7 {
+
+    private int x;
+    private int y;
+
+    A7() {
+        this.x =10 ;
+
+        }
+    A7(int y) {
+        this.();
+        this.y=y;
+    }
+}
