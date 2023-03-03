62ddb4a46afa6a95e999be4062c25e6af5da8575
Sreeginy Kodeeswaran <sreeginy98@gmail.com>
2023-03-03 23:38:30 +0530

    Create Member.java

---
+++ src/Example/Member.java
@@ -0,0 +1,11 @@
+package Example;
+
+public class Member {
+    String firstname;
+    String Lasname;
+    int age;
+
+    public void talk() {
+        System.out.println(firstname+Lasname+age);
+    }
+}
