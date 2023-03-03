5edba27c4f4fa38d00a19e52878fe614eb8970b2
Sreeginy Kodeeswaran <sreeginy98@gmail.com>
2023-03-03 23:40:29 +0530

    Update Member.java

--- src/Example/Member.java
+++ src/Example/Member.java
@@ -8,4 +8,11 @@ public class Member {
     public void talk() {
         System.out.println(firstname+Lasname+age);
     }
+
+    public static void main(String[] args) {
+        Member M = new Member();
+        M.age = 24;
+        M.firstname = "Giny";
+        M.Lasname = "Jos";
+    }
 }
